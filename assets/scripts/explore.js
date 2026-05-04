ea405652c3e787e5297b8210ce69370e7a0c9434
jsonwang2003 <jason0100w@gmail.com>
2026-05-04 14:16:00 -0700

    reset explore.js to default

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,27 +3,7 @@
 window.addEventListener('DOMContentLoaded', init); 
 
 function init() {
-  console.log("hello world");
-
-  const selector = document.getElementById("horn-select");
   // TODO
-  select.addEventListener('change', function() {
-    console.log(selector);
-    console.log(selector.firstChild.value);
-    // code to run when the event is triggered
-    if(selector.firstChild.value === "air-horn") {
-      document.querySelector("#explore img").src = "assets\images\air-horn.svg";
-      document.querySelector("audio").src = "assets\audio\air-horn.mp3";
-    }
-    else if(selector.firstChild.value === "car-horn") {
-      document.querySelector("#explore img").src = "assets\images\car-horn.svg";
-      document.querySelector("audio").src = "assets\audio\car-horn.mp3";
-    }
-    else { //it's the party horn
-      document.querySelector("#explore img").src = "assets\images\party-horn.svg";
-      document.querySelector("audio").src = "assets\audio\party-horn.mp3";
-    }
-  });
 }
 
 
